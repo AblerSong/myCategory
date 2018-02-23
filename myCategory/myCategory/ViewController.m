5a606fb38308efde69f5da975fd0c9fd09fbd47d
song <linksong@outlook.com>
2018-02-23 20:16:08 +0800

    分类的使用

--- myCategory/myCategory/ViewController.m
+++ myCategory/myCategory/ViewController.m
@@ -7,7 +7,8 @@
 //
 
 #import "ViewController.h"
-
+#import "UIButton+SYAdd.h"
+#import "UIAlertController+SYAdd.h"
 @interface ViewController ()
 
 @end
@@ -18,7 +19,21 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)changeImageAlignment:(id)sender {
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 150, 80)];
+    [button setImage:[UIImage imageNamed:@"clock"] forState:UIControlStateNormal];
+    [button setTitle:@"我在下面" forState:UIControlStateNormal];
+    button.backgroundColor = [UIColor greenColor];
+    [button changeButtonImageAlignment:SYImageAlignmentTop withSpace:10.0];// 设置好图片和文字之后调用该方法
+    [self.view addSubview:button];
+}
 
+- (IBAction)alertController:(id)sender {
+    UIAlertController *alertVC = [UIAlertController alertControllerWithTitle:@"我是title" message:@"我是message" preferredStyle:UIAlertControllerStyleActionSheet sureHandle:^(UIAlertAction *action) {
+        NSLog(@"确定");
+    }];
+    [self presentViewController:alertVC animated:YES completion:nil];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
